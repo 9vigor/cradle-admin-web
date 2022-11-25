0ef806a7c0f6d5b8bf886c8d2e95d968971b0dbb
panda <panda@dt.com>
2022-11-25 17:04:52 +0800

    update

--- src/utils/http/axios/index.ts
+++ src/utils/http/axios/index.ts
@@ -1,7 +1,7 @@
 // axios配置  可自行根据项目进行更改，只需更改该文件即可，其他文件可以不动
 // The axios configuration can be changed according to the project, just change the file, other files can be left unchanged
 
-import type { AxiosInstance, AxiosResponse, AxiosRequestConfig, axios } from 'axios';
+import type { AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios';
 import { clone } from 'lodash-es';
 import type { RequestOptions, Result } from '/#/axios';
 import type { AxiosTransform, CreateAxiosOptions } from './axiosTransform';
@@ -20,6 +20,7 @@ import { useUserStore, useUserStoreWithOut } from '/@/store/modules/user';
 import { AxiosRetry } from '/@/utils/http/axios/axiosRetry';
 import { refreshToken } from '/@/api/sys/user';
 import { LoginStateEnum, useLoginState } from '/@/views/sys/login/useLogin';
+import axios from 'axios';
 
 const globSetting = useGlobSetting();
 const urlPrefix = globSetting.urlPrefix;
